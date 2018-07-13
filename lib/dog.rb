e0bfc8e2595b724bd935e803a0325dcf7114f9ac
katesliang <kateliangg@gmail.com>
2018-07-13 14:04:25 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,20 @@
 # dog.rb
+class Dog
+
+  def name
+    @name
+  end
+
+  def name=(name)
+    @name = name
+    puts @name
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end
+end
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,18 @@
 # person.rb
+class Person
+  def job
+    @job
+  end
+  
+  def job=(job)
+    @job = job
+  end
+  
+   def name=(name)
+    @name = name
+  end
+  
+  def name
+    @name
+  end
+end
