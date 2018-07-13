173b0c0f5d8e28cea336262c4afb73f4aff8b291
ciciiiruiz <ciciiiruiz@gmail.com>
2018-07-13 18:52:24 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,26 @@
-# person.rb
+ class Person
+ 
+ def person
+    person = @person
+  end
+  
+  def person=(person)
+    @person = person
+  end
+  
+  def name
+    name = @name
+  end
+  
+  def name=(name)
+    @name = name
+  end
+  
+  def job
+    job = @job
+  end
+  
+  def job=(job)
+    @job = job
+  end
+end
